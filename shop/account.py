c986348f67873f678d94e0c39c4b22afd00c9cd3
Jaysel33t <softelele@mail.ru>
2020-07-24 22:13:47 +0300

    rm old folder

--- shop/account.py
+++
@@ -1,5 +0,0 @@
-from models import User
-
-def get_user_info(username):
-    user = User.query.filter_by(username = username).first()
-    return {"email" : user.email, "money" : user.money, "phone" : user.phone, "adress" : user.adress}
