b75108a3d3fb61b10c84bc75f6ccaa5652803537
Jaysel33t <softelele@mail.ru>
2020-07-28 15:24:44 +0300

    Change readme.md

--- flask/app/config.py
+++ flask/app/config.py
@@ -4,6 +4,7 @@ DEBUG = True
 if os.path.isfile("/app/app/database.db"):
     SQLALCHEMY_DATABASE_URI = "sqlite:////app/app/database.db"
 elif os.path.isfile("/home/jayse/flask_shop/db/database.db"):
+    # Setup this path for local running
     SQLALCHEMY_DATABASE_URI = "sqlite:////home/jayse/flask_shop/db/database.db"
     print("!!!! PATH TO DATABASE: " + SQLALCHEMY_DATABASE_URI + " !!!!")
 
